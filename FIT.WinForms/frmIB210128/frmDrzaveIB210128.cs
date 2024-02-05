10d4afedbb7d17cd8bf429844979a703b3c123e5
alemB1 <alem.belko@edu.fit.ba>
2024-02-05 17:03:38 +0100

    Add data & new form

---
+++ FIT.WinForms/frmIB210128/frmDrzaveIB210128.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIT.WinForms.frmIB210128
+{
+    public partial class frmDrzaveIB210128 : Form
+    {
+        public frmDrzaveIB210128()
+        {
+            InitializeComponent();
+        }
+    }
+}
